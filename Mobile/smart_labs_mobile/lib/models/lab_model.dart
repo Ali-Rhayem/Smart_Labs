92632f7dd33bdaae12c2de7833a668a6ea936c5b
Ali Rhayem <alirhayem.1212@gmail.com>
2025-01-04 21:23:16 +0200

    Add Lab model class with relevant fields and constructor

---
+++ Mobile/smart_labs_mobile/lib/models/lab_model.dart
@@ -0,0 +1,29 @@
+class Lab {
+  final String labId;
+  final String labCode;
+  final String labName;
+  final String description;
+  final String ppe;
+  final List<String> instructors;
+  final List<String> students;
+  final DateTime date;
+  final String startTime;
+  final String endTime;
+  final String report;
+  final String semesterId;
+
+  const Lab({
+    required this.labId,
+    required this.labCode,
+    required this.labName,
+    required this.description,
+    required this.ppe,
+    required this.instructors,
+    required this.students,
+    required this.date,
+    required this.startTime,
+    required this.endTime,
+    required this.report,
+    required this.semesterId,
+  });
+}
