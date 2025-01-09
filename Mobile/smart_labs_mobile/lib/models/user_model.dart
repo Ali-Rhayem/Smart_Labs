313b7761a71e61c2cf26b28303c6b6602ab3362d
Ali Rhayem <alirhayem.1212@gmail.com>
2025-01-09 22:37:24 +0200

    Add User model to represent user data and attributes

---
+++ Mobile/smart_labs_mobile/lib/models/user_model.dart
@@ -0,0 +1,23 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String password;
+  final String major;
+  final String faculty;
+  final String imageUrl;
+  final String role;
+  final String faceIdentityVector;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.major,
+    required this.faculty,
+    required this.imageUrl,
+    required this.role,
+    required this.faceIdentityVector,
+  });
+}
