c221148438d3e7363a68b39097edb3317b560def
Ali Rhayem <alirhayem.1212@gmail.com>
2025-01-17 00:14:40 +0200

    Refactor main.dart to load environment variables from .env file

--- Mobile/smart_labs_mobile/lib/main.dart
+++ Mobile/smart_labs_mobile/lib/main.dart
@@ -7,8 +7,10 @@ import 'package:smart_labs_mobile/screens/login.dart';
 import 'package:smart_labs_mobile/screens/student/student_main_wrapper.dart';
 import 'package:smart_labs_mobile/screens/student/student_dashboard.dart';
 import 'package:smart_labs_mobile/screens/student/student_labs.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
