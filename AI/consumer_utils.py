1a8b55c61232d5b3f2c92ad11b2cfe01c0bcd83f
MhmdRhayem <10121011@mu.edu.lb>
2025-01-16 21:43:12 +0200

    Add consumer_utils.py for image processing and model integration

---
+++ AI/consumer_utils.py
@@ -0,0 +1,10 @@
+from ultralytics import YOLO
+import base64
+from PIL import Image
+import numpy as np
+import cv2
+from facenet_pytorch import InceptionResnetV1
+from torchvision import transforms
+import torch
+import matplotlib.pyplot as plt
+import random
