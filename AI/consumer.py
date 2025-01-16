d0660ad018381569a77ce12c02acd140ef423304
MhmdRhayem <10121011@mu.edu.lb>
2025-01-16 22:06:06 +0200

    Load Kafka configuration from environment variables in consumer module

--- AI/consumer.py
+++ AI/consumer.py
@@ -6,3 +6,16 @@ import numpy as np
 from pprint import pprint
 import time
 from consumer_utils import *
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+conf = {
+    'bootstrap.servers': os.getenv("BOOTSTRAP_SERVERS"),
+    'group.id': 'python-consumer',
+    'auto.offset.reset': 'earliest'
+}
+
+consumer = Consumer(conf)
+
+consumer.subscribe(['test'])
