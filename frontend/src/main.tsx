b61bdb6f7eda31ff7c57135e1be9fe283fc58d17
JawadElSayed <jawadelsayed3@gmail.com>
2025-01-20 23:29:55 +0200

    frontend > add React Query for data fetching and wrap App component with QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,19 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ThemeProviderComponent from "./themes/ThemeContext";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 
 root.render(
-	<ThemeProviderComponent>
-		<App />
-	</ThemeProviderComponent>
+	<QueryClientProvider client={queryClient}>
+		<ThemeProviderComponent>
+			<App />
+		</ThemeProviderComponent>
+	</QueryClientProvider>
 );
