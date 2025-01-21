1c48fd854f1fb6600fa604ed45edcc37bff37fcc
JawadElSayed <jawadelsayed3@gmail.com>
2025-01-21 20:40:52 +0200

    frontend > add CSS variables for dark theme colors and update Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: "#FFFF00", // Neon Yellow
+				},
+				secondary: "#1C1C1C", // Slightly Lighter Dark Gray
+				background: {
+					dark: "#121212", // Dark Mode Background
+				},
+				text: {
+					dark: "#FFFFFF", // Dark Mode Text
+				},
+				card: {
+					dark: "#1C1C1C", // Dark Mode Card
+				},
+			},
+		},
 	},
 	plugins: [],
 };
