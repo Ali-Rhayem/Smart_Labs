4433ac35d37ce45d058d49aadd45eabfdb433fcc
JawadElSayed <jawadelsayed3@gmail.com>
2025-01-22 00:11:20 +0200

    backend > update UserService to handle user updates with validation and image processing

--- backend/Services/UserService.cs
+++ backend/Services/UserService.cs
@@ -6,15 +6,20 @@ namespace backend.Services;
 public class UserService
 {
     private readonly IMongoCollection<User> _users;
+    private readonly IMongoCollection<UpdateUser> _UpdateUser;
 
     public UserService(IMongoDatabase database)
     {
         _users = database.GetCollection<User>("Users");
+        _UpdateUser = database.GetCollection<UpdateUser>("Users");
     }
 
     // for testing purposes
-    public async Task<User> CreateUser(User user)
+    public async Task<User?> CreateUser(User user)
     {
+        var if_user = await GetUserByEmailAsync(user.Email);
+        if (if_user != null && if_user.Id != user.Id)
+            return null;
         var lastUser = _users.Find(_ => true).SortByDescending(user => user.Id).FirstOrDefault();
         var UserId = lastUser == null ? 1 : lastUser.Id + 1;
         user.Id = UserId;
@@ -43,21 +48,34 @@ public class UserService
             .Find(user => user.Id == id).Project<User>(projection).FirstOrDefaultAsync();
     }
 
-    public async Task<bool> UpdateUser(int id, User updatedFields)
+    public async Task<bool> UpdateUser(int id, UpdateUser updatedFields)
     {
-        var updateDefinition = new List<UpdateDefinition<User>>();
-        var builder = Builders<User>.Update;
-
-        // restricted fields
-        var restrictedFields = new HashSet<string> { "Id", "Email", "Password", "Role", "FaceIdentityVector" };
+        var updateDefinition = new List<UpdateDefinition<UpdateUser>>();
+        var builder = Builders<UpdateUser>.Update;
 
         foreach (var prop in updatedFields.GetType().GetProperties())
         {
             var value = prop.GetValue(updatedFields);
 
-            if (value != null && !restrictedFields.Contains(prop.Name))
+            if (value != null && prop.Name != "Id")
             {
                 var fieldName = prop.Name;
+                if (fieldName == "Image")
+                {
+                    var image = value as string;
+                    var imageType = image!.Split(';')[0].Split('/')[1];
+                    var imageBytes = Convert.FromBase64String(image.Split(',')[1]);
+                    var imagePath = $"/var/www/pfps/{id}.{imageType}";
+                    await File.WriteAllBytesAsync(imagePath, imageBytes);
+                    value = imagePath;
+                }
+                else if (fieldName == "email")
+                {
+                    var user = await GetUserByEmailAsync(value.ToString()!);
+                    if (user != null && user.Id != id)
+                        continue;
+                }
+
                 var fieldUpdate = builder.Set(fieldName, value);
                 updateDefinition.Add(fieldUpdate);
             }
@@ -66,8 +84,8 @@ public class UserService
         if (!updateDefinition.Any())
             return false;
 
-        var update = Builders<User>.Update.Combine(updateDefinition);
-        var result = await _users.UpdateOneAsync(user => user.Id == id, update);
+        var update = Builders<UpdateUser>.Update.Combine(updateDefinition);
+        var result = await _UpdateUser.UpdateOneAsync(user => user.Id == id, update);
 
         return result.IsAcknowledged && result.ModifiedCount > 0;
     }
