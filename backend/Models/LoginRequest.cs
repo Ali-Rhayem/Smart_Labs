0771267e153effe5aca04d867188453850d11e2a
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-02 20:06:23 +0200

    backend > add FCM token property to LoginRequest model for Firebase integration

--- backend/Models/LoginRequest.cs
+++ backend/Models/LoginRequest.cs
@@ -4,4 +4,5 @@ public class LoginRequest
 {
     public required string Email { get; set; }
     public required string Password { get; set; }
+    public required string? Fcm_token { get; set; }
 }
