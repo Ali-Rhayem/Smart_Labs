2c6f3226e3d2bf25a70b2a6e256f9861647afc5a
Ali Rhayem <alirhayem.1212@gmail.com>
2025-02-02 19:43:58 +0200

    Implement FCM token storage using SecureStorage

--- Mobile/smart_labs_mobile/lib/api/firebase_api.dart
+++ Mobile/smart_labs_mobile/lib/api/firebase_api.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:smart_labs_mobile/utils/secure_storage.dart';
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
   print('Title: ${message.notification?.title}');
@@ -8,11 +9,14 @@ Future<void> handleBackgroundMessage(RemoteMessage message) async {
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
+  final SecureStorage _secureStorage = SecureStorage();
 
   Future<void> initNotification() async {
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
-    print('Token: $fCMToken');
+    if (fCMToken != null) {
+      await _secureStorage.storeFcmToken(fCMToken);
+    }
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
   }
 }
