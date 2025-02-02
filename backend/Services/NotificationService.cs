3ca4dbbce2a52903c4cc84ad7b4c6c36aeee6227
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-02 02:01:46 +0200

    backend > add SendNotificationAsync method to NotificationService for sending notifications and saving them to the database

--- backend/Services/NotificationService.cs
+++ backend/Services/NotificationService.cs
@@ -18,6 +18,45 @@ public class NotificationService
         _users = user;
     }
 
+    public async Task SendNotificationAsync(NotificationModel notification)
+    {
+        var message = new MulticastMessage
+        {
+            Tokens = notification.TargetFcmTokens,
+            Notification = new Notification
+            {
+                Title = notification.Title,
+                Body = notification.Body
+            },
+            Data = notification.Data
+        };
+
+        var last_notification_id = _notifications.Find(Builders<Notifications>.Filter.Empty).SortByDescending(n => n.Id).FirstOrDefault()?.Id ?? 0;
+        foreach (var token in notification.TargetFcmTokens)
+        {
+            last_notification_id += 1;
+            User user = await _users.GetUserByFcmTokenAsync(token);
+            if (user != null)
+            {
+                var save_notification = new Notifications
+                {
+                    Id = last_notification_id,
+                    Title = notification.Title,
+                    Message = notification.Body,
+                    Data = notification.Data,
+                    Date = DateTime.Now,
+                    UserID = user.Id,
+                };
+                await _notifications.InsertOneAsync(save_notification);
+            }
+
+        }
+
+        var response = await FirebaseMessaging.DefaultInstance.SendEachForMulticastAsync(message);
+
+        Console.WriteLine("Successfully sent message: " + response);
+    }
+
     public async Task<Notifications> GetNotificationByIdAsync(int notificationId)
     {
         var filter = Builders<Notifications>.Filter.Eq(n => n.Id, notificationId);
