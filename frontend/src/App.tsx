070d840c386b062e2469dc97a69c5b7f9282f075
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-05 23:47:08 +0200

    frontend > add routing structure with role-based layouts and protected routes for application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,53 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import LabsPage from "./pages/LabsPage";
+import { Role } from "./config/routes";
+import PublicLayout from "./layouts/PublicLayout";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+import NotFoundPage from "./pages/NotFoundPage";
+import { UserProvider } from "./contexts/UserContext";
+import UnauthorizedPage from "./pages/UnauthorizedPage";
+import ProtectedRoute from "./contexts/ProtectedRoute";
+
+const currentUserRole: Role = "instructor";
 
 const App: React.FC = () => {
-	document.documentElement.setAttribute("data-theme","dark")
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<LoginPage />} />
-				<Route path="/labs" element={<LabsPage />} />
-			</Routes>
-		</Router>
+		<UserProvider>
+			<Router>
+				<Routes>
+					<Route element={<PublicLayout />}>
+						<Route path="/" element={<LoginPage />} />
+						<Route path="*" element={<NotFoundPage />} />
+						<Route
+							path="/unauthorized"
+							element={<UnauthorizedPage />}
+						/>
+					</Route>
+
+					<Route
+						element={<ProtectedLayout userRole={currentUserRole} />}
+					>
+						{/* <Route path="/" element={<DashboardPage />} /> */}
+						{/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+						<Route
+							path="/labs"
+							element={
+								<ProtectedRoute
+									requiredRoles={[
+										"student",
+										"instructor",
+										"admin",
+									]}
+								>
+									<LabsPage />
+								</ProtectedRoute>
+							}
+						/>
+					</Route>
+				</Routes>
+			</Router>
+		</UserProvider>
 	);
 };
 
