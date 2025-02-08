17a793befe3bdbd73ef6c747a3e578a0148af40a
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-08 13:30:31 +0200

    frontend > feat: add methods to retrieve students and instructors in a lab

--- frontend/src/services/labService.ts
+++ frontend/src/services/labService.ts
@@ -1,15 +1,27 @@
 import { smart_labs } from "../utils/axios";
 import { Lab, CreateLabDto } from "../types/lab";
+import { User } from "../types/user";
 
 export const labService = {
 	getLabs: () => smart_labs.getAPI<Lab[]>("/lab"),
+
 	getInstructerLabs: (id: number) =>
 		smart_labs.getAPI<Lab[]>(`/lab/instructor/${id}`),
+
 	getStudentLabs: (id: number) =>
 		smart_labs.getAPI<Lab[]>(`/lab/student/${id}`),
+
+	getStudentsInLab: (id: number) =>
+		smart_labs.getAPI<User[]>(`/lab/${id}/students`),
+
+	getInstructorsInLab: (id: number) =>
+		smart_labs.getAPI<User[]>(`/lab/${id}/instructors`),
+
 	createLab: (data: CreateLabDto) =>
 		smart_labs.postAPI<Lab, CreateLabDto>("/lab", data),
+
 	deleteLab: (id: number) => smart_labs.deleteAPI<void>(`/lab/${id}`),
+
 	updateLab: (id: number, data: Partial<CreateLabDto>) =>
 		smart_labs.putAPI<Lab, Partial<CreateLabDto>>(`/lab/${id}`, data),
 };
