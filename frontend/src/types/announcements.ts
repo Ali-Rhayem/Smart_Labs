29e509900ea1a38035b6704f846a8760809c35ff
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-10 21:46:19 +0200

    frontend > add AnnouncementDTO and CommentDTO interfaces for improved type safety

--- frontend/src/types/announcements.ts
+++ frontend/src/types/announcements.ts
@@ -1,15 +1,33 @@
+import { UserDTO } from "./user";
+
 export interface Comment {
-  id: number;
-  userId: number;
-  content: string;
-  time: Date;
+	id: number;
+	userId: number;
+	content: string;
+	time: Date;
 }
 
 export interface Announcement {
-  id: number;
-  sender: number;
-  message: string;
-  files: string[];
-  time: Date;
-  comments: Comment[];
+	id: number;
+	sender: number;
+	message: string;
+	files: string[];
+	time: Date;
+	comments: Comment[];
+}
+
+export interface AnnouncementDTO {
+	id?: number;
+	user: UserDTO;
+	message: string;
+	files: string[];
+	time: Date;
+	comments: CommentDTO[];
+}
+
+export interface CommentDTO {
+	id: number;
+	user: UserDTO;
+	message: string;
+	time: Date;
 }
