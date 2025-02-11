0272fe9b192710c9660b27205d75fb147062ce0b
MhmdRhayem <10121011@mu.edu.lb>
2025-02-11 22:44:31 +0200

    Refactor Code to use UTC time

--- AI/start_producer.py
+++ AI/start_producer.py
@@ -17,11 +17,11 @@ data = {
             "command": "end",
             # "encoding": images_to_base64(image_path),
             "ppe_arr": ["gloves", "goggles"],
-            "session_id": 1,
-            "lab_id": 1,
+            "session_id": 3,
+            "lab_id": 6,
             "room": "B-103",
             "date": datetime.date.today().strftime("%Y-%m-%d"),
-            "time": datetime.datetime.utcnow().strftime("%H:%M:%S")
+            "time": datetime.datetime.now(datetime.UTC).strftime("%H:%M:%S")
         }
 
 future = producer.send(topic, value=data)
