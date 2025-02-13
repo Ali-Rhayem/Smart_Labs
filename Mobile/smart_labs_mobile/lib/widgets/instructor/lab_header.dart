f8a70250a276adb6117b23625a281df2544991d3
Ali Rhayem <alirhayem.1212@gmail.com>
2025-02-13 22:26:14 +0200

    Remove unnecessary Flutter widgets import in LabHeader

--- Mobile/smart_labs_mobile/lib/widgets/instructor/lab_header.dart
+++ Mobile/smart_labs_mobile/lib/widgets/instructor/lab_header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:smart_labs_mobile/models/lab_model.dart';
 
 class LabHeader extends StatelessWidget {
