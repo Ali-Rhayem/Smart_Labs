19027fec3f95f33f5f6a74ad9cb3676c37ddb1b9
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-14 21:03:39 +0200

    backend > add Sessions and related DTOs for enhanced session data management and reporting

--- backend/Models/Sessions.cs
+++ backend/Models/Sessions.cs
@@ -1,4 +1,7 @@
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using Org.BouncyCastle.Security;
+using backend.Models;
 
 
 public class Sessions
@@ -17,6 +20,88 @@ public class Sessions
     public string? Report { get; set; }
 
     [BsonElement("result")]
-    private string? Result { get; set; }
+    public List<ObjectResult> Result { get; set; } = [];
 
+    [BsonElement("total_attendance")]
+    public decimal TotalAttendance { get; set; }
+
+    [BsonElement("total_ppe_compliance")]
+    public Dictionary<string, decimal> TotalPPECompliance { get; set; } = [];
+
+    [BsonElement("outputs")]
+    public List<Output> Outputs { get; set; } = [];
+
+    [BsonElement("total_attendance_bytime")]
+    public List<int> Total_attendance_bytime { get; set; } = [];
+
+    [BsonElement("ppe_compliance_bytime")]
+    public Dictionary<string, List<int>> PPE_compliance_bytime { get; set; } = [];
+
+    [BsonElement("total_ppe_compliance_bytime")] 
+    public List<int> Total_ppe_compliance_bytime { get; set; } = [];
+
+
+}
+
+public class ObjectResult
+{
+    [BsonId]
+    public int Id { get; set; }
+    [BsonElement("name")]
+    public string? Name { get; set; }
+    [BsonElement("attendance_percentage")]
+    public decimal Attendance_percentage { get; set; }
+    [BsonElement("ppe_compliance")]
+    public Dictionary<string, decimal> PPE_compliance { get; set; } = [];
+}
+
+public class Output
+{
+    [BsonElement("time")]
+    public string? Time { get; set; }
+    [BsonElement("people")]
+    public List<Person> People { get; set; } = new List<Person>();
+}
+
+public class Person
+{
+    [BsonElement("id")]
+    public int Person_id { get; set; }
+
+    [BsonElement("name")]
+    public string Name { get; set; } = "";
+
+    [BsonElement("ppe")]
+    public Dictionary<string, int> PPE { get; set; } = [];
+}
+
+public class SessionsDTO
+{
+    public int? Id { get; set; }
+
+    public int LabId { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public string? Report { get; set; }
+
+    public List<ObjectResultDTO> Result { get; set; } = [];
+
+    public decimal TotalAttendance { get; set; }
+
+    public Dictionary<string, decimal> TotalPPECompliance { get; set; } = [];
+
+    private List<Output> Outputs { get; set; } = [];
+    public List<int> Total_attendance_bytime { get; set; } = [];
+    public Dictionary<string, List<int>> PPE_compliance_bytime { get; set; } = [];
+    public List<int> Total_ppe_compliance_bytime { get; set; } = [];
+}
+
+public class ObjectResultDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public required UserDTO User { get; set; }
+    public decimal Attendance_percentage { get; set; }
+    public Dictionary<string, decimal> PPE_compliance { get; set; } = [];
 }
