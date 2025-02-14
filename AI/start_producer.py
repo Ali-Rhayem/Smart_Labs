51b3342c26f38e82a4d2a9c145ff8e3c9770f34a
MhmdRhayem <10121011@mu.edu.lb>
2025-02-14 22:55:41 +0200

    Update start_producer.py to use environment variables for Kafka topic and bootstrap servers; modify session and lab IDs

--- AI/start_producer.py
+++ AI/start_producer.py
@@ -1,24 +1,26 @@
 import json
-import os
 import datetime
 import time
 from kafka import KafkaProducer
 from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 producer = KafkaProducer(
-    bootstrap_servers="localhost:9092",
+    bootstrap_servers=os.getenv("BOOTSTRAP_SERVERS"),
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-topic = 'recording_se'
+topic = os.getenv("RECORDING_TOPIC")
 images_folder = "./test_images"
 
 data = {
             "command": "end",
             # "encoding": images_to_base64(image_path),
             "ppe_arr": ["gloves", "goggles"],
-            "session_id": 1,
-            "lab_id": 1,
+            "session_id": 3,
+            "lab_id": 6,
             "room": "B-103",
             # "date": datetime.date.today().strftime("%Y-%m-%d"),
             # "time": datetime.datetime.now(datetime.UTC).strftime("%H:%M:%S")
