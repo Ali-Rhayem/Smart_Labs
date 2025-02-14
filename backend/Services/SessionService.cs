9dfce3d4fabbb986f55b3c04dedd9fa54d35bc63
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-14 21:09:20 +0200

    backend > add SessionService to return structured session data with user details

--- backend/Services/SessionService.cs
+++ backend/Services/SessionService.cs
@@ -1,18 +1,55 @@
+using backend.Models;
+using backend.Services;
 using MongoDB.Driver;
 
 
 public class SessionService
 {
     private readonly IMongoCollection<Sessions> _sessions;
+    private readonly UserService _userService;
 
-    public SessionService(IMongoDatabase database)
+    public SessionService(IMongoDatabase database, UserService userService)
     {
         _sessions = database.GetCollection<Sessions>("Sessions");
+        _userService = userService;
     }
 
-    public async Task<List<Sessions>> GetSessionsOfLabAsync(int labId)
+    public async Task<List<SessionsDTO>> GetSessionsOfLabAsync(int labId)
     {
-        return await _sessions.Find(session => session.LabId == labId).ToListAsync();
+        var sessions = await _sessions.Find(session => session.LabId == labId).ToListAsync();
+        List<SessionsDTO> sessionsDTO = [];
+        foreach (Sessions session in sessions)
+        {
+            var sessionDTO = new SessionsDTO
+            {
+                Id = session.Id,
+                LabId = session.LabId,
+                Date = session.Date,
+                Report = session.Report,
+                TotalAttendance = session.TotalAttendance,
+                TotalPPECompliance = session.TotalPPECompliance,
+                Total_ppe_compliance_bytime = session.Total_ppe_compliance_bytime,
+                Total_attendance_bytime = session.Total_attendance_bytime,
+                PPE_compliance_bytime = session.PPE_compliance_bytime,
+            };
+            var result = new List<ObjectResultDTO>();
+            foreach (ObjectResult r in session.Result)
+            {
+                var user = (UserDTO)await _userService.GetUserById(r.Id);
+                var output = new ObjectResultDTO
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                    User = user,
+                    Attendance_percentage = r.Attendance_percentage,
+                    PPE_compliance = r.PPE_compliance
+                };
+                result.Add(output);
+            }
+            sessionDTO.Result = result;
+            sessionsDTO.Add(sessionDTO);
+        }
+        return sessionsDTO;
     }
 
     public async Task<Sessions> CreateSessionAsync(int labId)
