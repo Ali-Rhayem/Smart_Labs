f0d4656fa6fbf5b7c758f556adf49b32816332b4
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-15 19:25:55 +0200

    backend > update FirstLogin model to allow nullable Image property

--- backend/Models/FirstLogin.cs
+++ backend/Models/FirstLogin.cs
@@ -4,7 +4,7 @@ public class FirstLogin
     public required string Name { get; set; }
     public required string Faculty { get; set; }
     public required string Major { get; set; }
-    public string Image { get; set; } = "";
+    public string? Image { get; set; } = "";
     public required string Password { get; set; }
     public required string ConfirmPassword { get; set; }
     public bool First_login { get; set; } = true;
