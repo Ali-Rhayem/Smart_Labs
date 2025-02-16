93b39fcd18e532ee353c57fdc57635a328111434
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-16 19:22:31 +0200

    frontend > refactor axios utility to enhance deleteAPI method and improve type safety

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,41 +2,43 @@ import axios from "axios";
 import { baseUrl } from "../config/config";
 
 const apiClient = axios.create({
-  baseURL: baseUrl,
-  headers: {
-    "Content-Type": "application/json",
-  },
+	baseURL: baseUrl,
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+	(config) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 interface SmartLabs {
-  getAPI: <T>(api_url: string) => Promise<T>;
-  postAPI: <T, D>(api_url: string, api_data: D) => Promise<T>;
-  putAPI: <T, D>(api_url: string, api_data: D) => Promise<T>;
-  deleteAPI: <T>(api_url: string) => Promise<T>;
+	getAPI: <T>(api_url: string) => Promise<T>;
+	postAPI: <T, D>(api_url: string, api_data: D) => Promise<T>;
+	putAPI: <T, D>(api_url: string, api_data: D) => Promise<T>;
+	deleteAPI: <T, D = Record<string, never>>(api_url: string, api_data?: D) => Promise<T>;
 }
 
 export const smart_labs: SmartLabs = {
-  getAPI: <T>(api_url: string) =>
-    apiClient.get<T>(api_url).then((response) => response.data),
-  postAPI: <T, D>(api_url: string, api_data: D) =>
-    apiClient.post<T>(api_url, api_data).then((response) => response.data),
-  putAPI: <T, D>(api_url: string, api_data: D) =>
-    apiClient.put<T>(api_url, api_data).then((response) => response.data),
-  deleteAPI: <T>(api_url: string) =>
-    apiClient.delete<T>(api_url).then((response) => response.data),
+	getAPI: <T>(api_url: string) =>
+		apiClient.get<T>(api_url).then((response) => response.data),
+	postAPI: <T, D>(api_url: string, api_data: D) =>
+		apiClient.post<T>(api_url, api_data).then((response) => response.data),
+	putAPI: <T, D>(api_url: string, api_data: D) =>
+		apiClient.put<T>(api_url, api_data).then((response) => response.data),
+	deleteAPI: <T, D = Record<string, never>>(api_url: string, api_data?: D) =>
+		apiClient
+			.delete<T>(api_url, api_data ? { data: api_data } : undefined)
+			.then((response) => response.data),
 };
 
 export default apiClient;
