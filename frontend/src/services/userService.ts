ee51539add968c7eaa68afd9406138881dfb4d65
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-17 01:27:09 +0200

    frontend > add resetPassword function to userService for password reset functionality

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -6,11 +6,13 @@ export const userService = {
 
 	getUsers: () => smart_labs.getAPI<User[]>(`/user`),
 
-	createUser: (user: any) =>
-		smart_labs.postAPI(`/user`, user),
+	createUser: (user: any) => smart_labs.postAPI(`/user`, user),
 
 	editUser: (id: number, user: any) =>
 		smart_labs.putAPI<User, UpdateUserDto>(`/user/${id}`, user),
 
 	deleteUser: (id: number) => smart_labs.deleteAPI(`/user/${id}`),
+
+	resetPassword: (email: string) =>
+		smart_labs.postAPI<never, string>(`/user/resetPassword`, email),
 };
