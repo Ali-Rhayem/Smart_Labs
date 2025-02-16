d04463658895b411e9cd8b8f3c12599358175f98
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-16 14:27:38 +0200

    frontend > add CommentOnAnnouncement method to announcementService for posting comments on announcements

--- frontend/src/services/announcementService.ts
+++ frontend/src/services/announcementService.ts
@@ -1,10 +1,24 @@
 import { smart_labs } from "../utils/axios";
-import { Announcement, AnnouncementDTO } from "../types/announcements";
+import {
+	Announcement,
+	AnnouncementDTO,
+	CommentDTO,
+} from "../types/announcements";
 
 export const announcementService = {
 	sendAnnouncement: (lab_id: number, announcement: Announcement) =>
 		smart_labs.postAPI<AnnouncementDTO, Announcement>(
 			`/lab/${lab_id}/announcement`,
 			announcement
+		),
+
+	CommentOnAnnouncement: (
+		lab_id: number,
+		announcement_id: number,
+		comment: Comment
+	) =>
+		smart_labs.postAPI<CommentDTO, Comment>(
+			`/lab/${lab_id}/announcement/${announcement_id}/comment`,
+			comment
 		),
 };
