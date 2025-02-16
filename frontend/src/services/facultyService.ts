4ebf9c97247d259341ee2587c74650c30317da82
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-16 19:22:58 +0200

    frontend > add faculty management methods to facultyService for CRUD operations

--- frontend/src/services/facultyService.ts
+++ frontend/src/services/facultyService.ts
@@ -3,4 +3,19 @@ import { smart_labs } from "../utils/axios";
 
 export const facultyService = {
 	getFaculties: () => smart_labs.getAPI<faculty[]>("/faculty"),
+
+	addFaculty: (faculty: faculty) =>
+		smart_labs.postAPI<faculty, faculty>("/faculty", faculty),
+
+	updateFaculty: (id: number, faculty: faculty) =>
+		smart_labs.putAPI<faculty, faculty>(`/faculty/${id}`, faculty),
+
+	deleteFaculty: (id: number) =>
+		smart_labs.deleteAPI<faculty>(`/faculty/${id}`),
+
+	addMajor: (id: number, major: string) =>
+		smart_labs.postAPI<faculty, string>(`/faculty/${id}/major`, major),
+
+	deleteMajor: (id: number, major: string) =>
+		smart_labs.deleteAPI<faculty, string>(`/faculty/${id}/major/`, major),
 };
