7106fefeae96fb166c5880ce479d03051a27d31e
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-18 16:58:42 +0200

    backend > refactor DashboardService to support role-based lab retrieval in GetDashboardAsync method

--- backend/Services/DashboardService.cs
+++ backend/Services/DashboardService.cs
@@ -13,15 +13,24 @@ public class DashboardService
         _serviceProvider = serviceProvider;
     }
 
-    public async Task<Dictionary<string, object>> GetinstructorDashboardAsync(int id)
+    public async Task<Dictionary<string, object>> GetDashboardAsync(int id, string role)
     {
         var _labService = _serviceProvider.GetRequiredService<LabService>();
-        var labs = await _labService.GetInstructorLabsAsync(id);
-
         var result = new Dictionary<string, object>();
+        List<Lab> labs = [];
+        if (role == "instructor")
+            labs = await _labService.GetInstructorLabsAsync(id);
+        else if (role == "admin")
+            labs = await _labService.GetAllLabsAsync();
+        else if (role == "student")
+            labs = await _labService.GetStudentLabsAsync(id);
+        else
+            return result;
+
         List<Dictionary<string, object>> labs_result = [];
 
         int total_students = 0;
+        int total_sessions = 0;
         int total_labs = labs.Count;
         int avg_attandance = 0;
         int ppe_compliance = 0;
@@ -33,6 +42,8 @@ public class DashboardService
             total_students += lab.Students.Count;
             var labResult = await _labService.AnalyzeLabAsync(lab.Id);
             labs_result.Add(labResult);
+            if (labResult.ContainsKey("xaxis"))
+                total_sessions += ((List<string>)labResult["xaxis"]).Count;
             if (labResult.ContainsKey("total_attendance"))
                 avg_attandance += (int)labResult["total_attendance"];
             if (labResult.ContainsKey("total_ppe_compliance"))
@@ -40,9 +51,11 @@ public class DashboardService
 
         }
 
-
+        if (role == "student")
+            result["total_students"] = total_sessions;
+        else
+            result["total_students"] = total_students;
         result["labs"] = labs_result;
-        result["total_students"] = total_students;
         result["total_labs"] = total_labs;
         result["avg_attandance"] = avg_attandance / total_labs;
         result["ppe_compliance"] = ppe_compliance / total_labs;
