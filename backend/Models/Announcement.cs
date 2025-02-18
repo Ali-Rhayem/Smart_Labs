49a39936ab547c218bcb175f6b12d17f937dadc1
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-18 13:39:00 +0200

    backend > add Announcement and Submission models with properties for assignment management and grading

--- backend/Models/Announcement.cs
+++ backend/Models/Announcement.cs
@@ -19,4 +19,38 @@ public class Announcement
 
     [BsonElement("Comments")]
     public List<Comment> Comments { get; set; } = [];
+
+    [BsonElement("AssignmentId")]
+    public bool Assignment { get; set; } = false;
+
+    [BsonElement("can_submit")]
+    public bool CanSubmit { get; set; } = false;
+
+    [BsonElement("Deadline")]
+    public DateTime Deadline { get; set; } = DateTime.UtcNow.AddDays(1);
+
+    [BsonElement("Submissions")]
+    public List<Submission> Submissions { get; set; } = [];
+
+    [BsonElement("Grade")]
+    public int? Grade { get; set; }
+}
+
+
+public class Submission
+{
+    [BsonElement("UserId")]
+    public int UserId { get; set; }
+
+    [BsonElement("Message")]
+    public string? Message { get; set; }
+
+    [BsonElement("Files")]
+    public List<string> Files { get; set; } = [];
+
+    [BsonElement("Time")]
+    public DateTime Time { get; set; } = DateTime.UtcNow;
+
+    [BsonElement("Grade")]
+    public int? Grade { get; set; }
 }
