36efab5eee419e48d7f3997002ce26344a3b03f0
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-18 13:38:14 +0200

    backend > add DashboardService for instructor dashboard data aggregation and analysis

---
+++ backend/Services/DashboardService.cs
@@ -0,0 +1,53 @@
+using MongoDB.Driver;
+namespace backend.Services;
+
+public class DashboardService
+{
+
+
+    private readonly IServiceProvider _serviceProvider;
+
+
+    public DashboardService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task<Dictionary<string, object>> GetinstructorDashboardAsync(int id)
+    {
+        var _labService = _serviceProvider.GetRequiredService<LabService>();
+        var labs = await _labService.GetInstructorLabsAsync(id);
+
+        var result = new Dictionary<string, object>();
+        List<Dictionary<string, object>> labs_result = [];
+
+        int total_students = 0;
+        int total_labs = labs.Count;
+        int avg_attandance = 0;
+        int ppe_compliance = 0;
+
+
+
+        foreach (var lab in labs)
+        {
+            total_students += lab.Students.Count;
+            var labResult = await _labService.AnalyzeLabAsync(lab.Id);
+            labs_result.Add(labResult);
+            if (labResult.ContainsKey("total_attendance"))
+                avg_attandance += (int)labResult["total_attendance"];
+            if (labResult.ContainsKey("total_ppe_compliance"))
+                ppe_compliance += (int)labResult["total_ppe_compliance"];
+
+        }
+
+
+        result["labs"] = labs_result;
+        result["total_students"] = total_students;
+        result["total_labs"] = total_labs;
+        result["avg_attandance"] = avg_attandance / total_labs;
+        result["ppe_compliance"] = ppe_compliance / total_labs;
+        return result;
+    }
+
+
+}
