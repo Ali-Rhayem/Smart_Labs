84a16278691e07a79cd0a6a98e83ba4227e9329a
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-18 13:39:06 +0200

    backend > add AnnouncementDTO and SubmissionDTO models for assignment submissions and grading

--- backend/Models/AnnouncementDTO.cs
+++ backend/Models/AnnouncementDTO.cs
@@ -13,4 +13,29 @@ public class AnnouncementDTO
     public DateTime Time { get; set; }
 
     public List<CommentDTO> Comments { get; set; } = [];
+
+    public bool Assignment { get; set; } = false;
+
+    public bool CanSubmit { get; set; } = false;
+
+    public DateTime Deadline { get; set; }
+
+    public List<SubmissionDTO> Submissions { get; set; } = [];
+
+    public int? Grade { get; set; }
+}
+
+public class SubmissionDTO
+{
+    public int UserId { get; set; }
+
+    public required UserDTO User { get; set; }
+
+    public string? Message { get; set; }
+
+    public List<string> Files { get; set; } = [];
+
+    public DateTime Time { get; set; }
+
+    public int? Grade { get; set; }
 }
