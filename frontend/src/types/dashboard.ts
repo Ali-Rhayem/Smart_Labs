7932f3a1dc15beddc01ccd2f7cdd8c41192867e7
JawadElSayed <jawadelsayed3@gmail.com>
2025-02-19 13:31:03 +0200

    frontend > add dashboard types for lab and PPE compliance data structures

--- frontend/src/types/dashboard.ts
+++ frontend/src/types/dashboard.ts
@@ -1,7 +1,34 @@
+import { User, UserDTO } from "./user";
+
 export interface dashboard {
 	total_students: number;
-	labs: { [key: string]: object }[];
+	labs: Lab[];
 	total_labs: number;
 	avg_attandance: number;
 	ppe_compliance: number;
+}
+
+export interface Lab {
+	lab_id: number;
+	lab_name: string;
+	xaxis: string[];
+	total_attendance: number;
+	total_ppe_compliance: number;
+	ppe_compliance: {
+		[key: string]: number;
+	};
+	people: People[];
+}
+
+export interface PPEComplianceData {
+	name: string;
+	value: number;
+}
+
+export interface People {
+	id: number;
+	name: string;
+	attendance_percentage: number;
+	ppe_compliance: { [key: string]: number };
+	user: UserDTO;
 }
