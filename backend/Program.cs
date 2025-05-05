e4696b4b43d81de30dd1d685708040740520b972
JawadElSayed <jawadelsayed3@gmail.com>
2025-05-05 21:56:54 +0300

    backend > refactor Program.cs to improve service registration and CORS policy configuration

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,112 +7,117 @@ using backend.helpers;
 using FirebaseAdmin;
 using Google.Apis.Auth.OAuth2;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddSingleton<IMongoClient>(sp =>
+public class Program
 {
-    var mongoSettings = builder.Configuration.GetSection("MongoDB");
-    var connectionString = mongoSettings["ConnectionString"];
-    return new MongoClient(connectionString);
-});
-
-builder.Services.AddScoped(sp =>
-{
-    var mongoSettings = builder.Configuration.GetSection("MongoDB");
-    var client = sp.GetRequiredService<IMongoClient>();
-    var databaseName = mongoSettings["DatabaseName"];
-    return client.GetDatabase(databaseName);
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowReactApp", policy =>
+    public static void Main(string[] args)
     {
-        policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<UserService>();
-builder.Services.AddScoped<LabService>();
-builder.Services.AddScoped<SemesterService>();
-builder.Services.AddScoped<PPEService>();
-builder.Services.AddScoped<RoomService>();
-builder.Services.AddScoped<FacultyService>();
-builder.Services.AddScoped<SessionService>();
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<DashboardService>();
-builder.Services.AddScoped<LabHelper>();
-builder.Services.AddSingleton<KafkaProducer>();
-
-builder.Services.AddSingleton<JwtTokenHelper>();
-builder.Services.AddControllers();
-
-// Add JWT Authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
+        // Add services to the container.
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddSingleton<IMongoClient>(sp =>
         {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-            ValidAudience = builder.Configuration["JwtSettings:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!))
-        };
-    });
+            var mongoSettings = builder.Configuration.GetSection("MongoDB");
+            var connectionString = mongoSettings["ConnectionString"];
+            return new MongoClient(connectionString);
+        });
 
-builder.Services.AddAuthorization();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Enter 'Bearer' followed by a space and the token",
-        Name = "Authorization",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-    {
+        builder.Services.AddScoped(sp =>
         {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            var mongoSettings = builder.Configuration.GetSection("MongoDB");
+            var client = sp.GetRequiredService<IMongoClient>();
+            var databaseName = mongoSettings["DatabaseName"];
+            return client.GetDatabase(databaseName);
+        });
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowEverything", policy =>
             {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                policy.AllowAnyOrigin()
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+        });
+
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<ILabService, LabService>();
+        builder.Services.AddScoped<ISemesterService, SemesterService>();
+        builder.Services.AddScoped<IPPEService, PPEService>();
+        builder.Services.AddScoped<IRoomService, RoomService>();
+        builder.Services.AddScoped<ISessionService, SessionService>();
+        builder.Services.AddScoped<INotificationService, NotificationService>();
+        builder.Services.AddScoped<IFacultyService, FacultyService>();
+        builder.Services.AddScoped<IDashboardService, DashboardService>();
+        builder.Services.AddScoped<ILabHelper, LabHelper>();
+        builder.Services.AddSingleton<IKafkaProducer, KafkaProducer>();
+        builder.Services.AddSingleton<IJwtTokenHelper, JwtTokenHelper>();
+        builder.Services.AddControllers();
+
+        // Add JWT Authentication
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "Bearer"
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+                    ValidAudience = builder.Configuration["JwtSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]!))
+                };
+            });
+
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                Description = "Enter 'Bearer' followed by a space and the token",
+                Name = "Authorization",
+                Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+            {
+                {
+                    new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                    {
+                        Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                        {
+                            Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
                 }
-            },
-            Array.Empty<string>()
+            });
+        });
+        FirebaseApp.Create(new AppOptions
+        {
+            Credential = GoogleCredential.FromFile(builder.Configuration["Firebase:credentials_path"])
+        });
+        var app = builder.Build();
+
+        // add swagger
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-    });
-});
-FirebaseApp.Create(new AppOptions
-{
-    Credential = GoogleCredential.FromFile(builder.Configuration["Firebase:credentials_path"])
-});
-var app = builder.Build();
 
-// add swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        app.UseCors("AllowEverything");
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.UseStaticFiles();
+
+        app.Run();
+    }
 }
-
-app.UseCors("AllowReactApp");
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-app.UseStaticFiles();
-
-app.Run();
